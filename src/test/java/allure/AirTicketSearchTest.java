7bfdbc14d59f333b68cb87599affccffe77f5ae3
Marat <maratussr@gmail.com>
2023-07-04 22:00:02 +0400

    added layer&Microservice&Jira annotation

--- src/test/java/allure/AirTicketSearchTest.java
+++ src/test/java/allure/AirTicketSearchTest.java
@@ -2,10 +2,7 @@ package allure;
 
 import base.TestBase;
 import com.codeborne.selenide.Condition;
-import io.qameta.allure.AllureId;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Owner;
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +11,7 @@ import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
+@Layer("web_ui_2")
 public class AirTicketSearchTest extends TestBase {
 
     @Test
@@ -21,6 +19,8 @@ public class AirTicketSearchTest extends TestBase {
     @DisplayName("Search air ticket")
     @Story("Search air ticket")
     @Owner("allure8")
+    @Microservice("second service")
+    @JiraIssues({@JiraIssue("HOMEWORK-774")})
     @Feature("Search ticket")
     public void searchAirTicket() {
         step("Open main page", () -> {
--- src/test/java/allure/BusTicketSearchTest.java
+++ src/test/java/allure/BusTicketSearchTest.java
@@ -2,16 +2,14 @@ package allure;
 
 import base.TestBase;
 import com.codeborne.selenide.Condition;
-import io.qameta.allure.AllureId;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Owner;
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+@Layer("web_ui_1")
 public class BusTicketSearchTest extends TestBase {
 
     @Test
@@ -19,6 +17,8 @@ public class BusTicketSearchTest extends TestBase {
     @DisplayName("Search bus ticket")
     @Story("Search bus ticket")
     @Owner("allure8")
+    @Microservice("first service")
+    @JiraIssues({@JiraIssue("HOMEWORK-774")})
     @Feature("Search ticket")
     public void searchBusTicket() {
         step("Open main page", () -> {
